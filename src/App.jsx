ae2cc0da2eefba7514afb90be28fc6abbb08b3f5
giacoletti <giacoletti92@gmail.com>
2021-12-17 15:19:00 +0100

    Adds index.css with some styling properties and includes it in index.js refactors quiz form from App.jsx into new component CreateQuizForm.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import Quizzes from "./modules/Quizzes";
-import CategorySelector from "./components/Categoryselector";
-import DifficultySelector from "./components/Difficultyselector";
+import React from "react";
+import { useSelector } from "react-redux";
+import CreateQuizForm from "./components/CreateQuizForm";
 
 const App = () => {
-  const dispatch = useDispatch();
   const { quiz } = useSelector(state => state)
-  const [category, setCategory] = useState();
-  const [difficulty, setDifficulty] = useState();
-
-  const createQuiz = async () => {
-    dispatch(Quizzes.create({
-      category: category,
-      difficulty: difficulty,
-    }));
-  };
 
   let questionList;
 
@@ -28,13 +16,7 @@ const App = () => {
   return (
     <>
       {!quiz.questions &&
-        <div data-cy="create-form">
-          <CategorySelector onCategoryChange={setCategory} />
-          <DifficultySelector onDifficultyChange={setDifficulty} />
-          <button data-cy="create-button" onClick={createQuiz}>
-            Create Quiz
-          </button>
-        </div>
+        <CreateQuizForm />
       }
       <div data-cy="quiz-list">{questionList}</div>
     </>
