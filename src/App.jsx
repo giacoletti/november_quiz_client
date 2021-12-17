71bead7a067efcc09e046c5d3766119bb17b2226
giacoletti <giacoletti92@gmail.com>
2021-12-17 16:11:15 +0100

    Extracts question display from App.jsx into new Question.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import CreateQuizForm from "./components/CreateQuizForm";
+import Question from './components/Question';
 
 const App = () => {
-  const { quiz } = useSelector(state => state)
-
-  let questionList;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const { quiz } = useSelector(state => state);
+  let questionUI;
 
   if (quiz.questions) {
-    questionList = quiz.questions.map((question, index) => {
-      return <li key={index}>{question.question}</li>;
-    });
+    let question = quiz.questions[currentQuestion]
+    questionUI =
+      currentQuestion >= quiz.questions.length ? <h1 style={{color: 'white'}}>You are done!</h1> :
+        <Question
+          currentQuestion={currentQuestion}
+          question={question}
+          incrementQuestion={setCurrentQuestion} />
   }
 
   return (
     <>
-      {!quiz.questions &&
-        <CreateQuizForm />
+      {!quiz.questions ?
+        <CreateQuizForm /> :
+        <div data-cy="quiz-list" className="quiz-container">
+          <div className="question box">
+            {questionUI}
+          </div>
+        </div>
       }
-      <div data-cy="quiz-list">{questionList}</div>
     </>
   );
 };
